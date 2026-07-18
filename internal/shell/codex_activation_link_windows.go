9abf1c746720d2ac51889c07de2a54e3d3822700
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-07-18 13:09:53 +1000

    fix Codex activation on Windows ARM64

--- internal/shell/codex_activation_link_windows.go
+++ internal/shell/codex_activation_link_windows.go
@@ -3,6 +3,7 @@
 package shell
 
 import (
+	"path/filepath"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
@@ -34,7 +35,25 @@ func replaceCodexActivationLink(src, dst string) error {
 	}
 	defer windows.CloseHandle(handle)
 
-	dstName, err := windows.UTF16FromString(dst)
+	dstDirName, err := windows.UTF16PtrFromString(filepath.Dir(dst))
+	if err != nil {
+		return err
+	}
+	dstDir, err := windows.CreateFile(
+		dstDirName,
+		windows.FILE_LIST_DIRECTORY,
+		windows.FILE_SHARE_READ|windows.FILE_SHARE_WRITE|windows.FILE_SHARE_DELETE,
+		nil,
+		windows.OPEN_EXISTING,
+		windows.FILE_FLAG_BACKUP_SEMANTICS,
+		0,
+	)
+	if err != nil {
+		return err
+	}
+	defer windows.CloseHandle(dstDir)
+
+	dstName, err := windows.UTF16FromString(filepath.Base(dst))
 	if err != nil {
 		return err
 	}
@@ -43,6 +62,7 @@ func replaceCodexActivationLink(src, dst string) error {
 	buffer := make([]byte, int(unsafe.Offsetof(layout.fileName))+nameBytes)
 	info := (*codexFileRenameInformation)(unsafe.Pointer(&buffer[0]))
 	info.flags = windows.FILE_RENAME_REPLACE_IF_EXISTS | windows.FILE_RENAME_POSIX_SEMANTICS
+	info.rootDirectory = dstDir
 	info.fileNameLength = uint32(nameBytes)
 	copy((*[windows.MAX_LONG_PATH]uint16)(unsafe.Pointer(&info.fileName[0]))[:len(dstName)-1], dstName)
 	return windows.SetFileInformationByHandle(handle, windows.FileRenameInfoEx, &buffer[0], uint32(len(buffer)))
